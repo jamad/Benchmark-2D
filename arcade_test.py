58599b6de5d56a6eea194b1cacab07462c3e77d2
jamad <jamad2001@gmail.com>
2024-08-27 00:25:54 +0300

    Update arcade_test.py

--- arcade_test.py
+++ arcade_test.py
@@ -15,15 +15,14 @@ class スプライト(arcade.Sprite):
         self.texture = choice(images)
         self.angle = 0
         self.rot_vel = randrange(-SPEED, SPEED)
-        self.vel_x, self.vel_y = randrange(-SPEED, SPEED), randrange(-SPEED, SPEED)
+        self.vel_x = randrange(-SPEED, SPEED)
+        self.vel_y = randrange(-SPEED, SPEED)
 
-    def update(self, delta_time=0):
+    def update(self, delta_time):
         self.angle += self.rot_vel * delta_time
         self.position = (self.position[0] + self.vel_x * delta_time, self.position[1] + self.vel_y * delta_time)
-        if not (0 <= self.position[0] <= 画面幅):
-            self.vel_x *= -1
-        if not (0 <= self.position[1] <= 画面高):
-            self.vel_y *= -1
+        if not (0 <= self.position[0] <= 画面幅):self.vel_x *= -1
+        if not (0 <= self.position[1] <= 画面高):self.vel_y *= -1
 
 class 俺アプリ(arcade.Window):
     経過時間 = 0.0
@@ -39,23 +38,20 @@ class 俺アプリ(arcade.Window):
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
             self.スプライト数 += クリック毎の生成数
-            for _ in range(クリック毎の生成数):
-                self.sprites.append(スプライト(self.images, x, y))
+            for _ in range(クリック毎の生成数): self.sprites.append(スプライト(self.images, x, y))
         elif button == arcade.MOUSE_BUTTON_RIGHT:
             count = min(クリック毎の生成数, len(self.sprites))
             self.スプライト数 -= count
-            for _ in range(count):
-                self.sprites.pop()
+            for _ in range(count):self.sprites.pop()
 
     def on_update(self, delta_time):
-        self.sprites.update()
-        self.経過時間 = delta_time
+        self.sprites.update(delta_time)
+        self.FPS = 1//delta_time
 
     def on_draw(self):
         self.clear()
         self.sprites.draw()
-        self.文字.text = f'{int(1 // self.経過時間)} FPS | {self.スプライト数} SPRITES'
+        self.文字.text = f'{self.FPS} FPS | {self.スプライト数} SPRITES'
         self.文字.draw()
 
-app = 俺アプリ()
-app.run()
+俺アプリ().run()
